1aacbaefa5b910ff5755603fa1f5c6c15e57fb47
redoyr393-ship-it <redoyr393@gmail.com>
2025-11-05 00:13:57 +0600

    25-62754-2

---
+++ Lab/LabTask4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LabTask4 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Problem 4: Swap Two Integers (Arithmetic Only)===");
+        System.out.print("Enter first integer (a):");
+        int a = scanner.nextInt();
+        System.out.print("Enter second integer (b):");
+        int b = scanner.nextInt();
+    }
+    
+
+
+}
