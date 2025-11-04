5a63e3fa0cbf76b07b4b397ab6b0b322acc055bc
redoyr393-ship-it <redoyr393@gmail.com>
2025-11-04 21:18:20 +0600

    25-62754-2

--- Lab/labtask1.java
+++ Lab/labtask1.java
@@ -1,3 +1,20 @@
 public class labtask1 {
+    public static void main(String[] args) {
+        // problem 1: Avarage of three student scores
+        System.out.println("=== problem 1: Avarage Score ===");
+        System.out.print("Enter score of student 1:");
+        double score1 = scanner.nextDouble();
+        System.out.print("Enter score of student 2:;");
+        double score2 = scanner.nextDouble();
+        System.out.print("Enter score of student 3:");
+        double score = scanner.nextDouble();
+
+        double avarage = (score1 + score2 + score3) / 3.0;
+        Symtem.out.printf("Average score: %.2f\n\n:", average);
+
+        scanner.close();
+    }
+    
+
     
 }
